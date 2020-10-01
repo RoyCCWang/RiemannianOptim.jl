c875e5a21446fdd0338316198199618e3cd02b80
RoyCCWang <61167584+RoyCCWang@users.noreply.github.com>
2020-10-01 01:12:17 -0400

    update

--- src/RiemannianOptim.jl
+++ src/RiemannianOptim.jl
@@ -17,8 +17,10 @@ include("../src/retractions/interval.jl")
 
 include("../src/manifold/vector_transport.jl")
 
-include("../src/optimization/CG.jl")
 include("../src/optimization/Rp/engine_Rp.jl")
+include("../src/optimization/vectorspace/engine_array.jl")
+
+include("../src/optimization/CG.jl")
 include("../src/optimization/TRS/trustregion.jl")
 include("../src/optimization/TRS/trhelpers.jl")
 
